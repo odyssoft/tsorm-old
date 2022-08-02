15bd00326301d7b52ca55e5d4d11997c84579fc1
SeannHewitson <seannkiely@icloud.com>
2022-08-02 20:17:01 +0100

    changing machine

--- src/types/join.ts
+++ src/types/join.ts
@@ -4,6 +4,8 @@ type JoinOptions<T> = {
   [Key in KeyOf<T>]?: number | null | OperatorType<T> | OperatorType<T>[]
 }
 
-export type Join<T, S = any> = 
+// export type J
+
+// export type Join<T, S = any> =
 
 // export type Join<T> = Or<JoinOptions<T>> | JoinOptions<T>
