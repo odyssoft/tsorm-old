4285849ec8b173b3f0629041d2a27e0b2d3ea268
SeannHewitson <seannkiely@icloud.com>
2022-08-02 20:17:05 +0100

    changing machine

--- src/types/delete.ts
+++ src/types/delete.ts
@@ -1,21 +1,5 @@
-import User, { IUser } from 'example/models/user'
-import { AliasModelKeys, Where } from './'
+import { Where } from './'
 
 export type DeleteOptions<T, A = any> = Where<T, A>
 
 export type Delete<T> = (options: DeleteOptions<T>) => void
-
-User.as<'u'>('u').delete({
-  $or: [
-    {
-      'u.age': {
-        $between: { min: 0, max: 10 },
-      },
-    },
-    {
-      'u.name': {
-        $like: '%foo%',
-      },
-    },
-  ],
-})
