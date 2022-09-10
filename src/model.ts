eb3c56092efb9b0d2d24d8f6feaa320580af4b5b
SeannHewitson <seannkiely@icloud.com>
2022-09-10 23:05:20 +0100

    TSORM-12: feat - Added base methods

--- src/model.ts
+++ src/model.ts
@@ -10,10 +10,51 @@ export function createModel<T>(name: string, keys: ModelKeys<T>) {
       return {} as T
     }
 
-    public static find() {}
-    public static findBy(key: KeyOf<T>, value: any) {}
-    public static findById() {}
-    public static findOne() {}
+    public static create(): T {
+      return {} as T
+    }
+    public static createMany(): T[] {
+      return [{}] as T[]
+    }
+
+    public static deleteMany(): number {
+      return 0
+    }
+    public static deleteOne(): boolean {
+      return false
+    }
+    public static deleteBy(): boolean {
+      return false
+    }
+    public static deleteById(): boolean {
+      return false
+    }
+
+    public static find(): T | T[] {
+      return {} as T
+    }
+    public static findBy(key: KeyOf<T>, value: any): T | T[] {
+      return {} as T
+    }
+    public static findById(): T {
+      return {} as T
+    }
+    public static findOne(): T {
+      return {} as T
+    }
+    public static findOneBy(): T {
+      return {} as T
+    }
+
+    public static updateMany(): T[] {
+      return [{}] as T[]
+    }
+    public static updateOne(): T {
+      return {} as T
+    }
+    public static updateOneBy(): T {
+      return {} as T
+    }
   }
 }
 
