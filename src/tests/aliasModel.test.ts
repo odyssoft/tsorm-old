d496b0350305e8fd5fb7fe93f7408388d4f36219
SeannHewitson <seannkiely@icloud.com>
2022-10-21 12:15:24 +0100

    TSORM-6: feat - Added alias for individual joins to shorten the number of lines required to use

--- src/tests/aliasModel.test.ts
+++ src/tests/aliasModel.test.ts
@@ -206,6 +206,116 @@ describe('aliasModel', () => {
         `SELECT u.userId, u.username, u.email, p.postId, p.userId, p.post FROM \`user\` AS u INNER JOIN \`post\` AS p ON u.userId = p.userId WHERE u.userId = 1 AND p.postId BETWEEN 1 AND 10 GROUP BY u.userId, p.postId ORDER BY u.userId, p.postId LIMIT 10`
       )
     })
+
+    describe('crossJoin', () => {
+      it('should return correct join sql with correct combined keys', () => {
+        const crossJoin = User.as('u').crossJoin(Post.as('p'), { 'u.userId': 'p.userId' })
+
+        expect(crossJoin.alias).toBe('u')
+        expect(crossJoin.joins).toEqual(['CROSS JOIN `post` AS p ON u.userId = p.userId'])
+        expect(crossJoin.keys).toEqual([
+          'u.userId',
+          'u.username',
+          'u.email',
+          'u.password',
+          'p.postId',
+          'p.userId',
+          'p.post',
+        ])
+      })
+    })
+
+    describe('innerJoin', () => {
+      it('should return correct join sql with correct combined keys', () => {
+        const innerJoin = User.as('u').innerJoin(Post.as('p'), { 'u.userId': 'p.userId' })
+
+        expect(innerJoin.alias).toBe('u')
+        expect(innerJoin.joins).toEqual(['INNER JOIN `post` AS p ON u.userId = p.userId'])
+        expect(innerJoin.keys).toEqual([
+          'u.userId',
+          'u.username',
+          'u.email',
+          'u.password',
+          'p.postId',
+          'p.userId',
+          'p.post',
+        ])
+      })
+    })
+
+    describe('leftJoin', () => {
+      it('should return correct join sql with correct combined keys', () => {
+        const leftJoin = User.as('u').leftJoin(Post.as('p'), { 'u.userId': 'p.userId' })
+
+        expect(leftJoin.alias).toBe('u')
+        expect(leftJoin.joins).toEqual(['LEFT JOIN `post` AS p ON u.userId = p.userId'])
+        expect(leftJoin.keys).toEqual([
+          'u.userId',
+          'u.username',
+          'u.email',
+          'u.password',
+          'p.postId',
+          'p.userId',
+          'p.post',
+        ])
+      })
+    })
+
+    describe('leftOuterJoin', () => {
+      it('should return correct join sql with correct combined keys', () => {
+        const leftOuterJoin = User.as('u').leftOuterJoin(Post.as('p'), { 'u.userId': 'p.userId' })
+
+        expect(leftOuterJoin.alias).toBe('u')
+        expect(leftOuterJoin.joins).toEqual(['LEFT OUTER JOIN `post` AS p ON u.userId = p.userId'])
+        expect(leftOuterJoin.keys).toEqual([
+          'u.userId',
+          'u.username',
+          'u.email',
+          'u.password',
+          'p.postId',
+          'p.userId',
+          'p.post',
+        ])
+      })
+    })
+
+    describe('rightJoin', () => {
+      it('should return correct join sql with correct combined keys', () => {
+        const rightJoin = User.as('u').rightJoin(Post.as('p'), { 'u.userId': 'p.userId' })
+
+        expect(rightJoin.alias).toBe('u')
+        expect(rightJoin.joins).toEqual(['RIGHT JOIN `post` AS p ON u.userId = p.userId'])
+        expect(rightJoin.keys).toEqual([
+          'u.userId',
+          'u.username',
+          'u.email',
+          'u.password',
+          'p.postId',
+          'p.userId',
+          'p.post',
+        ])
+      })
+    })
+
+    describe('rightOuterJoin', () => {
+      it('should return correct join sql with correct combined keys', () => {
+        const rightOuterJoin = User.as('u').rightOuterJoin(Post.as('p'), { 'u.userId': 'p.userId' })
+
+        expect(rightOuterJoin.alias).toBe('u')
+        expect(rightOuterJoin.joins).toEqual([
+          'RIGHT OUTER JOIN `post` AS p ON u.userId = p.userId',
+        ])
+        expect(rightOuterJoin.keys).toEqual([
+          'u.userId',
+          'u.username',
+          'u.email',
+          'u.password',
+          'p.postId',
+          'p.userId',
+          'p.post',
+        ])
+      })
+    })
   })
 
   describe('SQL', () => {
